c11ef11d86959d93a7abfe74f6baa91ec3d84387
CourtneyWai <140558208+Work-4-Water@users.noreply.github.com>
2025-06-04 12:28:09 -1000

    Topic 1 2025

---
+++ story_content/triggers.js
@@ -0,0 +1,71 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "5hZySJjCbU2":
+        Script1();
+        break;
+      case "6bNiXPR0aXS":
+        Script2();
+        break;
+  }
+}
+
+window.InitExecuteScripts = function()
+{
+var player = GetPlayer();
+var object = player.object;
+var once = player.once;
+var addToTimeline = player.addToTimeline;
+var setVar = player.SetVar;
+var getVar = player.GetVar;
+var update = player.update;
+var pointerX = player.pointerX;
+var pointerY = player.pointerY;
+var showPointer = player.showPointer;
+var hidePointer = player.hidePointer;
+var slideWidth = player.slideWidth;
+var slideHeight = player.slideHeight;
+window.Script1 = function()
+{
+  player.once(() => {
+const target = object('6U5KWfqF0pt');
+const duration = 750;
+const easing = 'ease-out';
+const id = '6nO5VfRaHkB';
+const pulseAmount = 0.03;
+const delay = 0;
+addToTimeline(
+target.animate([
+{ scale: '1' }, { scale: `${1 + pulseAmount}` },
+{ scale: '1' }, { scale: `${1 + pulseAmount}` },
+{ scale: '1' }
+],
+  { fill: 'forwards', delay, duration, easing }
+), id
+);
+});
+}
+
+window.Script2 = function()
+{
+  player.once(() => {
+const target = object('67kpL45GIaw');
+const duration = 750;
+const easing = 'ease-out';
+const id = '6ceLmhCieor';
+const pulseAmount = 0.03;
+const delay = 0;
+addToTimeline(
+target.animate([
+{ scale: '1' }, { scale: `${1 + pulseAmount}` },
+{ scale: '1' }, { scale: `${1 + pulseAmount}` },
+{ scale: '1' }
+],
+  { fill: 'forwards', delay, duration, easing }
+), id
+);
+});
+}
+
+};
